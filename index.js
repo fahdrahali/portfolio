427d9d65416b22a01f106b649a73049bcdfa561b
Munezero Christian <chamsf5@aol.com>
2022-02-28 17:24:52 +0200

    Add displayMenu javascript function

---
+++ index.js
@@ -0,0 +1,10 @@
+function displayMenu() {
+    let mobileNavMenu = document.getElementById("mobile-menu");
+    let mobileNav = document.getElementById("mobile-nav");
+
+        mobileNavMenu.style.display = "flex";
+        mobileNav.style.display = "none";  
+
+}
+
+
