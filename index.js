dc611f9cdbcbff872c388650ea186fefa674af44
Fahd Rahali <rahali.fahd@gmail.com>
2022-03-02 02:18:01 +0100

    build the html css and javascript desktop popup

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 function displayMenu() {
   const mobileNavMenu = document.getElementById('mobile-menu');
   const mobileNav = document.getElementById('mobile-nav');
@@ -8,7 +9,6 @@ function displayMenu() {
 function hideMenu() {
   const mobileNavMenu = document.getElementById('mobile-menu');
   const mobileNav = document.getElementById('mobile-nav');
-
   mobileNavMenu.style.display = 'none';
   mobileNav.style.display = 'block';
 }
@@ -29,7 +29,36 @@ function displayContactSection() {
   hideMenu();
 }
 
-document.addEventListener('DOMContentLoaded', displayMenu);
-document.addEventListener('DOMContentLoaded', displayContactSection);
-document.addEventListener('DOMContentLoaded', displayAboutSection);
-document.addEventListener('DOMContentLoaded', displayHomeSection);
+function hidePopup() {
+  const hiddenPopupDesktop = document.getElementById('popup');
+  hiddenPopupDesktop.classList.toggle('hide');
+  document.getElementById('container').classList.remove('bg-container-popup');
+  document.querySelector('.popup-badge-list').innerHTML = '';
+  document.querySelector('works').scrollIntoView({ block: 'start' });
+}
+
+const project = {
+  img: "url('./assets/images/popup-image.png')",
+  title: 'Keeping track of hundreds of components',
+  skills: ['Codekit', 'GitHub', 'Javascript', 'Bootstrap', 'Terminal', 'Codepen'],
+  description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus vel veniam illo quod facilis consequatur odio eaque, commodi eligendi? Repellat ratione, provident corporis cumque reprehenderit consectetur excepturi obcaecati dignissimos dolorem.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus vel veniam illo quod facilis consequatur odio eaque, commodi eligendi? Repellat ratione.',
+};
+
+document.querySelectorAll('.btn-projects').forEach((btn) => {
+  btn.addEventListener('click', () => {
+    const hiddenPopupDesktop = document.getElementById('popup');
+    document.querySelector('.popup-desktop-image').style.backgroundImage = project.img;
+    document.querySelector('.title-popup-desktop').innerHTML = project.title;
+    const list = document.querySelector('.popup-badge-list');
+
+    for (let i = 0; i < project.skills.length; i += 1) {
+      const li = document.createElement('li');
+      li.appendChild(document.createTextNode(project.skills[i]));
+      list.appendChild(li);
+    }
+    document.querySelector('.popup-desktop-content').innerHTML = project.description;
+
+    hiddenPopupDesktop.classList.remove('hide');
+    document.getElementById('container').classList.add('bg-container-popup');
+  });
+});
